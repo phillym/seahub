3f2f5db62d653ba352054e9f406d2f92b5477286
Daniel Pan <freeplant@gmail.com>
2017-04-06 16:14:06 +0800

    Revert "update get org repo owner"

--- seahub/api2/endpoints/admin/libraries.py
+++ seahub/api2/endpoints/admin/libraries.py
@@ -26,12 +26,11 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 def get_repo_info(repo):
-
     repo_owner = seafile_api.get_repo_owner(repo.repo_id)
-    try:
+
+    org_repo_owner = None
+    if hasattr(seafile_api, 'get_org_repo_owner'):
         org_repo_owner = seafile_api.get_org_repo_owner(repo.repo_id)
-    except Exception:
-        org_repo_owner = None
 
     result = {}
     result['id'] = repo.repo_id
